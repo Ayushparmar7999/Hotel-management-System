8beef8fb6f176eb49f14a16364dbdbf5546cb3f5
Ayush Parmar <150412427+Ayushparmar7999@users.noreply.github.com>
2024-03-16 23:05:12 +0530

    init

---
+++ HMS/krishna/views.py
@@ -0,0 +1,441 @@
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse , HttpResponseRedirect
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.core.mail import send_mail  
+# Create your views here.
+
+#homepage
+def homepage(request):
+    all_location = Hotels.objects.values_list('location','id').distinct().order_by()
+    if request.method =="POST":
+        try:
+            print(request.POST)
+            hotel = Hotels.objects.all().get(id=int(request.POST['search_location']))
+            rr = []
+            for each_reservation in Reservation.objects.all():
+                if str(each_reservation.check_in) < str(request.POST['cin']) and str(each_reservation.check_out) < str(request.POST['cout']):
+                    pass
+                elif str(each_reservation.check_in) > str(request.POST['cin']) and str(each_reservation.check_out) > str(request.POST['cout']):
+                    pass
+                else:
+                    rr.append(each_reservation.room.id)
+                
+            room = Rooms.objects.all().filter(hotel=hotel,capacity__gte = int(request.POST['capacity'])).exclude(id__in=rr)
+            if len(room) == 0:
+                messages.warning(request,"Sorry No Rooms Are Available on this time period")
+            data = {'rooms':room,'all_location':all_location,'flag':True}
+            response = render(request,'book.html',data)
+        except Exception as e:
+            messages.error(request,e)
+            response = render(request,'index.html',{'all_location':all_location})
+    else:
+        data = {'all_location':all_location}
+        response = render(request,'index.html',data)
+    return HttpResponse(response)
+
+#about
+def aboutpage(request):
+    return HttpResponse(render(request,'about.html'))
+
+#contact page
+def contactpage(request):
+    return HttpResponse(render(request,'contact.html'))
+
+def OurRooms(request):
+    return render(request,'room.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+#user sign up
+def user_sign_up(request):
+    if request.method =="POST":
+        user_name = request.POST['username']
+        
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.warning(request,"Password didn't matched")
+            return redirect('userloginpage')
+        
+        try:
+            if User.objects.all().get(username=user_name):
+                messages.warning(request,"Username Not Available")
+                return redirect('userloginpage')
+        except:
+            pass
+        new_user = User.objects.create_user(username=user_name,password=password1)
+        new_user.is_superuser=False
+        new_user.is_staff=False
+        new_user.save()
+        messages.success(request,"Registration Successfull")
+        return redirect("userloginpage")
+    return HttpResponse('Access Denied')
+
+#staff sign up
+def staff_sign_up(request):
+    if request.method =="POST":
+        user_name = request.POST['username']
+        
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.success(request,"Password didn't Matched")
+            return redirect('staffloginpage')
+        try:
+            if User.objects.all().get(username=user_name):
+                messages.warning(request,"Username Already Exist")
+                return redirect("staffloginpage")
+        except:
+            pass
+        
+        new_user = User.objects.create_user(username=user_name,password=password1)
+        new_user.is_superuser=True
+        new_user.is_staff=True
+        new_user.save()
+        messages.success(request," Staff Registration Successfull")
+        return redirect("staffloginpage")
+    else:
+
+        return HttpResponse('Access Denied')
+#user login and signup page
+def user_log_sign_page(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pswd']
+
+        user = authenticate(username=email,password=password)
+        try:
+            if user.is_staff:
+                
+                messages.error(request,"Incorrect username or Password")
+                return redirect('staffloginpage')
+        except:
+            pass
+        
+        if user is not None:
+            login(request,user)
+            messages.success(request,"successful logged in")
+            print("Login successfull")
+            return redirect('homepage')
+        else:
+            messages.warning(request,"Incorrect username or password")
+            return redirect('userloginpage')
+
+    response = render(request,'user/userlogsign.html')
+    return HttpResponse(response)
+
+#logout for admin and user 
+def logoutuser(request):
+    if request.method =='GET':
+        logout(request)
+        messages.success(request,"Logged out successfully")
+        print("Logged out successfully")
+        return redirect('homepage')
+    else:
+        print("logout unsuccessfull")
+        return redirect('userloginpage')
+
+#staff login and signup page
+def staff_log_sign_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username,password=password)
+        
+        if user.is_staff:
+            login(request,user)
+            return redirect('staffpanel')
+        
+        else:
+            messages.success(request,"Incorrect username or password")
+            return redirect('staffloginpage')
+    response = render(request,'staff/stafflogsign.html')
+    return HttpResponse(response)
+
+#staff panel page
+@login_required(login_url='/staff')
+def panel(request):
+    
+    if request.user.is_staff == False:
+        return HttpResponse('Access Denied')
+    
+    rooms = Rooms.objects.all()
+    total_rooms = len(rooms)
+    available_rooms = len(Rooms.objects.all().filter(status='1'))
+    unavailable_rooms = len(Rooms.objects.all().filter(status='2'))
+    reserved = len(Reservation.objects.all())
+    
+    available=total_rooms-reserved
+
+    hotel = Hotels.objects.values_list('location','id').distinct().order_by()
+
+    response = render(request,'staff/panel.html',{'location':hotel,'reserved':reserved,'rooms':rooms,'total_rooms':total_rooms,'available':available,'unavailable':unavailable_rooms})
+    return HttpResponse(response)
+
+#for editing room information
+@login_required(login_url='/staff')
+def edit_room(request):
+    if request.user.is_staff == False:   
+        return HttpResponse('Access Denied')
+    if request.method == 'POST' and request.user.is_staff:
+        print(request.POST)
+        old_room = Rooms.objects.all().get(id= int(request.POST['roomid']))
+        hotel = Hotels.objects.all().get(id=int(request.POST['hotel']))
+        old_room.room_type  = request.POST['roomtype']
+        old_room.capacity   =int(request.POST['capacity'])
+        old_room.price      = int(request.POST['price'])
+        old_room.size       = int(request.POST['size'])
+        old_room.hotel      = hotel
+        old_room.status     = request.POST['status']
+        old_room.room_number=int(request.POST['roomnumber'])
+
+        old_room.save()
+        messages.success(request,"Room Details Updated Successfully")
+        return redirect('staffpanel')
+    else:
+        room_id = request.GET['roomid']
+        room = Rooms.objects.all().get(id=room_id)
+        response = render(request,'staff/editroom.html',{'room':room})
+        return HttpResponse(response)
+
+#for adding room
+@login_required(login_url='/staff')
+def add_new_room(request):
+    if request.user.is_staff == False:
+        return HttpResponse('Access Denied')
+    if request.method == "POST":
+        total_rooms = len(Rooms.objects.all())
+        new_room = Rooms()
+        hotel = Hotels.objects.all().get(id = int(request.POST['hotel']))
+        print(f"id={hotel.id}")
+        print(f"name={hotel.name}")
+
+
+        new_room.roomnumber = total_rooms + 1
+        new_room.room_type  = request.POST['roomtype']
+        new_room.capacity   = int(request.POST['capacity'])
+        new_room.size       = int(request.POST['size'])
+        new_room.capacity   = int(request.POST['capacity'])
+        new_room.hotel      = hotel
+        new_room.status     = request.POST['status']
+        new_room.price      = request.POST['price']
+
+        new_room.save()
+        messages.success(request,"New Room Added Successfully")
+    
+    return redirect('staffpanel')
+
+#booking room page
+@login_required(login_url='/user')
+def book_room_page(request):
+    room = Rooms.objects.all().get(id=int(request.GET['roomid']))
+    return HttpResponse(render(request,'user/bookroom.html',{'room':room}))
+
+#For booking the room
+@login_required(login_url='/user')
+def book_room(request):
+    
+    if request.method =="POST":
+
+        room_id = request.POST['room_id']
+        
+        room = Rooms.objects.all().get(id=room_id)
+        #for finding the reserved rooms on this time period for excluding from the query set
+        for each_reservation in Reservation.objects.all().filter(room = room):
+            if str(each_reservation.check_in) < str(request.POST['check_in']) and str(each_reservation.check_out) < str(request.POST['check_out']):
+                pass
+            elif str(each_reservation.check_in) > str(request.POST['check_in']) and str(each_reservation.check_out) > str(request.POST['check_out']):
+                pass
+            else:
+                messages.warning(request,"Sorry This Room is unavailable for Booking")
+                return redirect("homepage")
+        
+        current_user = request.user
+        total_person = int( request.POST['person'])
+        booking_id = str(room_id) + str(datetime.datetime.now())
+
+        reservation = Reservation()
+        room_object = Rooms.objects.all().get(id=room_id)
+        room_object.status = '2'
+        
+        user_object = User.objects.all().get(username=current_user)
+        reservation.guest = user_object
+        reservation.room = room_object
+        person = total_person
+        reservation.check_in = request.POST['check_in']
+        reservation.check_out = request.POST['check_out']
+
+        reservation.save()
+
+        messages.success(request,"Congratulations! Booking Successfull")
+
+        return redirect("homepage")
+    else:
+        return HttpResponse('Access Denied')
+
+# def handler404(request):
+#     return render(request, '404.html', status=404)
+
+@login_required(login_url='/staff')   
+def view_room(request):
+    room_id = request.GET['roomid']
+    room = Rooms.objects.all().get(id=room_id)
+
+    reservation=Reservation.objects.all().filter(room=room)
+
+    return HttpResponse(render(request,'staff/viewroom.html',{'room':room,'reservations':reservation}))
+
+@login_required(login_url='/user')
+def user_bookings(request):
+    if request.user.is_authenticated == False:
+        return redirect('userloginpage')
+    user = User.objects.all().get(id=request.user.id)
+    print(f"request user id ={request.user.id}")
+    bookings = Reservation.objects.all().filter(guest=user)
+    if not bookings:
+        messages.warning(request,"No Bookings Found")
+    return HttpResponse(render(request,'user/mybookings.html',{'bookings':bookings}))
+
+@login_required(login_url='/staff')
+def add_new_location(request):
+    if request.method == "POST" and request.user.is_staff:
+        owner = request.POST['new_owner']
+        location = request.POST['new_city']
+        state = request.POST['new_state']
+        country = request.POST['new_country']
+        hotels = Hotels.objects.all().filter(location = location , state = state)
+        if hotels:
+            messages.warning(request,"Sorry City at this Location already exist")
+            return redirect("staffpanel")
+        else:
+            new_hotel = Hotels()
+            new_hotel.owner = owner
+            new_hotel.location = location
+            new_hotel.state = state
+            new_hotel.country = country
+            new_hotel.save()
+            messages.success(request,"New Location Has been Added Successfully")
+            return redirect("staffpanel")
+
+    else:
+        return HttpResponse("Not Allowed")
+    
+#for showing all bookings to staff
+@login_required(login_url='/staff')
+def all_bookings(request):
+    bookings = Reservation.objects.all()
+
+    if not bookings:
+        messages.warning(request,"No Bookings Found")
+    return HttpResponse(render(request,'staff/allbookings.html',{'bookings':bookings}))
+    
+
+def delete(request):
+    if request.user.is_staff == False:   
+        return HttpResponse('Access Denied')
+    if request.method == 'POST' and request.user.is_staff:
+        print(request.POST)
+        old_room = Rooms.objects.all().get(id= int(request.POST['roomid']))
+        hotel = Hotels.objects.all().get(id=int(request.POST['hotel']))
+        old_room.room_type  = request.POST['roomtype']
+        old_room.capacity   =int(request.POST['capacity'])
+        old_room.price      = int(request.POST['price'])
+        old_room.size       = int(request.POST['size'])
+        old_room.hotel      = hotel
+        old_room.status     = request.POST['status']
+        old_room.room_number=int(request.POST['roomnumber'])
+
+        old_room.save()
+        messages.success(request,"Room Details Updated Successfully")
+        return redirect('staffpanel')
+    else:
+        room_id = request.GET['roomid']
+        room = Rooms.objects.all().get(id=room_id)
+        response = render(request,'staff/editroom.html',{'room':room})
+        return HttpResponse(response)
+
+
+def mail(request):
+    if request.method=="POST":
+        Name = request.POST['Name']
+        Email = request.POST['Email']
+        Phone_Number = request.POST['Phone_Number']
+        msg = request.POST['message']
+
+        subject="Test_Mail from Django Server"
+        msg1={"Name":Name,
+                "Email":Email,
+                "Phone Number":Phone_Number,
+                "message":msg}
+
+        from_email="vaibhvamalviya89@gmail.com"
+        message = str(msg1)
+        recipient_list=["vaibhavmalviya1999@outlook.com"]
+        send_mail(subject,message,from_email,recipient_list)
+    return render(request,'index.html')
+
+
+
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+from .models import ItemModel
+
+
+def payment(request,nm,pr):
+    # fm = PaymentForm()
+    return render(request,'user/payment.html',{'nm':nm,'pr':pr})
+
+def item_payment(request,nm,pr):
+    if request.method=="POST":
+        name = request.POST['name']
+        amount = int(request.POST['amount']) * 100
+        client = razorpay.Client(auth=("rzp_test_WacsBqwCGgnP1N","fkazi8QbTdceAkWWONziJE0H"))
+        response_payment = client.order.create({'amount':amount, 'currency':'INR','payment_capture':'1' })
+
+        order_status = response_payment['status']
+        order_id = response_payment['id']
+        
+        if order_status=='created':
+            product = ItemModel(name=name , amount =amount , order_id = response_payment['id'])
+            product.save()
+            response_payment['name'] = name
+            # fm = PaymentForm( request.POST or None)
+            return render(request,'user/payment.html',{'payment':response_payment,'nm':nm,'pr':pr})
+
+   
+    return render(request,'user/payment.html')
+
+@csrf_exempt
+def paymentStatus(request):
+    # print(request.POST)
+    if request.method=='POST':
+        response = request.POST
+        # print(response)
+        params_dict = {
+            'razorpay_order_id': response['razorpay_order_id'],
+            'razorpay_payment_id': response['razorpay_payment_id'],
+            'razorpay_signature': response['razorpay_signature']
+        }
+        # client instance
+        client = razorpay.Client(auth=("rzp_test_WacsBqwCGgnP1N","fkazi8QbTdceAkWWONziJE0H"))
+        try:
+            status = client.utility.verify_payment_signature(params_dict)
+            item = ItemModel.objects.get(order_id=response['razorpay_order_id'])
+            item.razorpay_payment_id = response['razorpay_payment_id']
+            item.paid = True
+            item.save()
+            # print("save all data in model")
+            return render(request, 'user/payment_status.html', {'status': True})
+        except:
+            # print("Not save all data in model")
+            return render(request, 'user/payment_status.html', {'status': False})
+    return render(request, 'user/payment_status.html')  
+
